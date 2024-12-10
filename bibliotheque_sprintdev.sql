1a57803aa06739957a0f7561a3dca066115bab4e
Lucas Ferreira <lucas.ferreira@edu.ece.fr>
2024-12-10 11:54:07 +0100

    changement de BDD

--- bibliotheque_sprintdev.sql
+++ bibliotheque_sprintdev.sql
@@ -1,214 +1,104 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le : mar. 10 déc. 2024 à 11:51
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de données : `bibliotheque_sprintdev`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `admin`
---
-
-CREATE TABLE `admin` (
-  `id_admin` int(11) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `mot_de_passe` varchar(255) NOT NULL,
-  `date_creation` timestamp NOT NULL DEFAULT current_timestamp()
+CREATE TABLE admin (
+  id_admin int(11) NOT NULL,
+  nom varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  mot_de_passe varchar(255) NOT NULL,
+  date_creation timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `admin`
---
 
-INSERT INTO `admin` (`id_admin`, `nom`, `email`, `mot_de_passe`, `date_creation`) VALUES
+INSERT INTO admin (id_admin, nom, email, mot_de_passe, date_creation) VALUES
 (1, 'Lucas', 'lucas.ferreira@edu.ece.fr', 'LucasF94430!', '0000-00-00 00:00:00');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `emprunt`
---
-
-CREATE TABLE `emprunt` (
-  `id_emprunt` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `id_livre` int(11) NOT NULL,
-  `date_emprunt` date NOT NULL DEFAULT curdate(),
-  `date_retour` date DEFAULT NULL
+CREATE TABLE emprunt (
+  id_emprunt int(11) NOT NULL,
+  id_user int(11) NOT NULL,
+  id_livre int(11) NOT NULL,
+  date_emprunt date NOT NULL DEFAULT curdate(),
+  date_retour date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `genre`
---
-
-CREATE TABLE `genre` (
-  `id_genre` int(11) NOT NULL,
-  `nom` varchar(255) DEFAULT NULL
+CREATE TABLE genre (
+  id_genre int(11) NOT NULL,
+  nom varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `genre`
---
-
-INSERT INTO `genre` (`id_genre`, `nom`) VALUES
+INSERT INTO genre (id_genre, nom) VALUES
 (1, 'Roman'),
 (2, 'Manga'),
 (3, 'Science-Fiction'),
 (4, 'Thriller');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `livre`
---
-
-CREATE TABLE `livre` (
-  `id_livre` int(10) NOT NULL,
-  `nom_livre` varchar(255) DEFAULT NULL,
-  `genre_livre` varchar(255) DEFAULT NULL,
-  `auteur_livre` varchar(255) DEFAULT NULL,
-  `annee_livre` int(10) DEFAULT NULL,
-  `tome_livre` int(10) DEFAULT NULL,
-  `etat_livre` varchar(255) NOT NULL DEFAULT 'Disponible'
+CREATE TABLE livre (
+  id_livre int(10) NOT NULL,
+  nom_livre varchar(255) DEFAULT NULL,
+  genre_livre varchar(255) DEFAULT NULL,
+  auteur_livre varchar(255) DEFAULT NULL,
+  annee_livre int(10) DEFAULT NULL,
+  tome_livre int(10) DEFAULT NULL,
+  etat_livre varchar(255) NOT NULL DEFAULT 'Disponible'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `livre`
---
-
-INSERT INTO `livre` (`id_livre`, `nom_livre`, `genre_livre`, `auteur_livre`, `annee_livre`, `tome_livre`, `etat_livre`) VALUES
+INSERT INTO livre (id_livre, nom_livre, genre_livre, auteur_livre, annee_livre, tome_livre, etat_livre) VALUES
 (32, 'Le Comte de Monte-Cristo', 'Manga', 'Alexandre Dumas', 112, 11, 'Disponible'),
 (35, 'Les Misérables', 'Roman', 'Victor Hugo', 2024, 111, 'Réservé');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
-CREATE TABLE `users` (
-  `id_user` int(11) NOT NULL,
-  `nom` varchar(100) NOT NULL,
-  `prenom` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `date_inscription` date NOT NULL DEFAULT curdate(),
-  `username` varchar(30) DEFAULT NULL
+CREATE TABLE users (
+  id_user int(11) NOT NULL,
+  nom varchar(100) NOT NULL,
+  prenom varchar(100) NOT NULL,
+  email varchar(100) NOT NULL,
+  password varchar(255) NOT NULL,
+  date_inscription date NOT NULL DEFAULT curdate(),
+  username varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`id_user`, `nom`, `prenom`, `email`, `password`, `date_inscription`, `username`) VALUES
+INSERT INTO users (id_user, nom, prenom, email, password, date_inscription, username) VALUES
 (8, '', '', 'lucas.ferreira@edu.ece.fr', '$2y$12$LhULrmSmS/90vAAdMAC9MOKzmOY469cPB.XUXE8iaeIpPKNchCELu', '2024-12-10', 'lulu'),
 (9, '', '', 'dgood@gmail.com', '$2y$12$8ZrHH2eOv7g74yiLFs8yVeMoJRsIEv84KlDKXCcaF/XZWNLZHuC1i', '2024-12-10', 'dgood');
 
---
--- Index pour les tables déchargées
---
+ALTER TABLE admin
+  ADD PRIMARY KEY (id_admin),
+  ADD UNIQUE KEY email (email);
 
---
--- Index pour la table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id_admin`),
-  ADD UNIQUE KEY `email` (`email`);
+ALTER TABLE emprunt
+  ADD PRIMARY KEY (id_emprunt),
+  ADD KEY id_user (id_user),
+  ADD KEY id_livre (id_livre);
 
---
--- Index pour la table `emprunt`
---
-ALTER TABLE `emprunt`
-  ADD PRIMARY KEY (`id_emprunt`),
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_livre` (`id_livre`);
+ALTER TABLE genre
+  ADD PRIMARY KEY (id_genre);
 
---
--- Index pour la table `genre`
---
-ALTER TABLE `genre`
-  ADD PRIMARY KEY (`id_genre`);
+ALTER TABLE livre
+  ADD PRIMARY KEY (id_livre);
 
---
--- Index pour la table `livre`
---
-ALTER TABLE `livre`
-  ADD PRIMARY KEY (`id_livre`);
+ALTER TABLE users
+  ADD PRIMARY KEY (id_user),
+  ADD UNIQUE KEY email (email);
 
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_user`),
-  ADD UNIQUE KEY `email` (`email`);
+ALTER TABLE admin
+  MODIFY id_admin int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT pour les tables déchargées
---
+ALTER TABLE emprunt
+  MODIFY id_emprunt int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT pour la table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE genre
+  MODIFY id_genre int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT pour la table `emprunt`
---
-ALTER TABLE `emprunt`
-  MODIFY `id_emprunt` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE livre
+  MODIFY id_livre int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
---
--- AUTO_INCREMENT pour la table `genre`
---
-ALTER TABLE `genre`
-  MODIFY `id_genre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE users
+  MODIFY id_user int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
---
--- AUTO_INCREMENT pour la table `livre`
---
-ALTER TABLE `livre`
-  MODIFY `id_livre` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
-
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `emprunt`
---
-ALTER TABLE `emprunt`
-  ADD CONSTRAINT `emprunt_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE,
-  ADD CONSTRAINT `emprunt_ibfk_2` FOREIGN KEY (`id_livre`) REFERENCES `livre` (`id_livre`) ON DELETE CASCADE;
+ALTER TABLE emprunt
+  ADD CONSTRAINT emprunt_ibfk_1 FOREIGN KEY (id_user) REFERENCES users (id_user) ON DELETE CASCADE,
+  ADD CONSTRAINT emprunt_ibfk_2 FOREIGN KEY (id_livre) REFERENCES livre (id_livre) ON DELETE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
